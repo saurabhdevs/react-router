484ff2622b0e345f193f052fab48895cc3e5949f
devsaurabhs <devs.saurabhs@gmail.com>
2018-04-30 11:22:41 +0530

    degin pattern committed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import $ from 'jquery';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class Fetch extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            content:[]
+        }
+    }
+    componentDidMount(){
+        $.ajax({
+            url:this.props.url,
+            success:(data)=>{
+            this.setState({
+                content:data
+            })
+            },
+            error:(error)=>{
+            console.log("error",error);
+            }
+        })
+    }
+    render(){
+        return(
+            <section>
+                {this.props.children(this.state.content)}
+            </section>
+        )
+    }
+}
+
+class App extends React.Component{
+    render(){
+        return(
+            <section>
+                <Fetch url = "http://jsonplaceholder.typicode.com/posts">
+                {(data)=>{
+                  return data.map((value)=>{
+                      return <li key = {value.id}>{value.title}</li>
+                  })
+                }}
+                </Fetch>
+                User:
+                <Fetch url = "http://jsonplaceholder.typicode.com/users">
+                {(data)=>{
+                  return data.map((value)=>{
+                      return <li key = {value.id}>{value.name}</li>
+                  })
+                }}
+                </Fetch> 
+
+            </section>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
